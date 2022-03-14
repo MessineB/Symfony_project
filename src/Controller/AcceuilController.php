77a996d60b4f430b81a18ec78e42a922a004bf71
MessineB <messinebelarousi@gmail.com>
2022-03-14 14:52:11 +0100

    footer header

---
+++ src/Controller/AcceuilController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AcceuilController extends AbstractController
+{
+    #[Route('/', name: 'main_page')]
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+}
---
+++ src/Controller/PostController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Post;
+use Symfony\Component\HttpFoundation\Response;
+
+class PostController extends AbstractController {
+
+    
+}
