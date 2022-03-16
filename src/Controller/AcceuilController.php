14db80c232dcad428253bfa57223e434f84811d1
MessineB <messinebelarousi@gmail.com>
2022-03-16 13:42:38 +0100

    form post

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
 use App\Repository\PostRepository;
-
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,13 +14,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AcceuilController extends AbstractController
 {
     #[Route('/', name: 'main_page')]
-    public function index(PostRepository $posts): Response
-    {
+    public function index(PostRepository $posts , EntityManagerInterface $manager , Request $request): Response
+    {   $post = new Post();
+        $form = $this->createForm(PostType::class , $post)
+        ->handleRequest($request);
+
+        if ( $form->isSubmitted() && $form->isValid()) {
+            $post->setCreatedAt(new \DateTimeImmutable());
+            $post->setStatus("affiché");
+            $post->setUser($this->getUser());
+            $manager->persist($post);
+            $manager->flush();
+        }
+
         return $this->render('index.html.twig', [
-            "posts" => $posts->findByDate() 
+            "posts" => $posts->findByDate() ,
+            "form" => $form->createView()
         ]);
     }
+    // #[Route('/ajoutlikepost', name: 'ajoutlikepost')]
+    // public function index() {
+    //     return 
+    // }
 }
 
 // POUR LA FONCTION D'AURELIEN !
-//<a href="{{path("show_post", "id":post.id) }}" > Voir post </a> 
+// <a href="{{path("NOM_DE_LA_PAGE", "id":post.id) }}" > Voir post </a> 
--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -50,7 +50,7 @@ class Authenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->urlGenerator->generate('main_page'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
